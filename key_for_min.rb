52a9c0d8fd1a6b9bca21842e82d083d179475c3f
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-08 18:23:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,8 @@ def key_for_min_value(name_hash)
 smallest_key = nil
 smallest_value = nil
 name_hash.each do |key, value|
-if 
+if smallest_value == nil || value < smallest_value
+      smallest_value = value
+      smallest_key = key
   end
 end
