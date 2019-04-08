b1a5653794af219ed148e76232f528d530312508
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-08 17:54:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+smallest = ""
+passengers.each do |key, value|
+  if value < value
+    smallest = name
+  end
+end
 
+end
+
+if a == b
+    0
+  elsif a < b
+    -1
+  elsif a > b
+    1
+  end
 end
