80d1aa429f8352edb09a1eaee3ee0311eb4ad03d
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-08 17:55:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,18 +4,14 @@
 def key_for_min_value(name_hash)
 smallest = ""
 passengers.each do |key, value|
-  if value < value
-    smallest = name
-  end
-end
-
-end
-
-if a == b
+if value == value
     0
-  elsif a < b
+  elsif value < value
     -1
-  elsif a > b
+  elsif value > value
     1
+  end
+  smallest = name
+  return smallest
   end
 end
